ac61cac0dfb8c0d043d4abbe9f58a2e83b33baa6
tristanpineau <tristan.pineau@ynov.com>
2018-04-24 17:15:49 +0200

    Travaux Swift

---
+++ CryptoGame/CryptoGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoGame
+//
+//  Created by Tristan Pineau on 24/04/2018.
+//  Copyright © 2018 Tristan Pineau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
